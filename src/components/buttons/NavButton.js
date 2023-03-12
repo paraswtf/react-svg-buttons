b8bcd0719e87c06d209b2dc94cfae6408507d53a
Paras Deshpande <63962807+paraswtf@users.noreply.github.com>
2023-03-12 16:36:43 +0530

    Fix defaultprops error react

--- src/components/buttons/NavButton.js
+++ src/components/buttons/NavButton.js
@@ -6,75 +6,70 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import MorphIcon from '../MorphIcon'
-import omit from '../../lib/omit'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import MorphIcon from "../MorphIcon";
+import omit from "../../lib/omit";
 
 class NavButton extends Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props);
 
-        this.handleMouseEnter = this.handleMouseEnter.bind(this)
-        this.handleMouseLeave = this.handleMouseLeave.bind(this)
+		this.handleMouseEnter = this.handleMouseEnter.bind(this);
+		this.handleMouseLeave = this.handleMouseLeave.bind(this);
 
-        this.state = { hover: false }
-    }
+		this.state = { hover: false };
+	}
 
-    handleMouseEnter() {
-        this.setState({ hover: true })
-    }
+	handleMouseEnter() {
+		this.setState({ hover: true });
+	}
 
-    handleMouseLeave() {
-        this.setState({ hover: false })
-    }
+	handleMouseLeave() {
+		this.setState({ hover: false });
+	}
 
-    render() {
-        const { direction, opened } = this.props
-        const { hover } = this.state
+	render() {
+		const { direction = "down", opened = false } = this.props;
+		const { hover } = this.state;
 
-        let type
-        if (hover) {
-            switch (direction) {
-                case 'up':
-                    type = opened ? 'arrowDown' : 'arrowUp'
-                    break
+		let type;
+		if (hover) {
+			switch (direction) {
+				case "up":
+					type = opened ? "arrowDown" : "arrowUp";
+					break;
 
-                case 'right':
-                    type = opened ? 'arrowLeft' : 'arrowRight'
-                    break
+				case "right":
+					type = opened ? "arrowLeft" : "arrowRight";
+					break;
 
-                case 'down':
-                    type = opened ? 'arrowUp' : 'arrowDown'
-                    break
+				case "down":
+					type = opened ? "arrowUp" : "arrowDown";
+					break;
 
-                case 'left':
-                    type = opened ? 'arrowRight' : 'arrowLeft'
-                    break
-            }
-        } else {
-            type = opened ? 'cross' : 'bars'
-        }
+				case "left":
+					type = opened ? "arrowRight" : "arrowLeft";
+					break;
+			}
+		} else {
+			type = opened ? "cross" : "bars";
+		}
 
-        return (
-            <MorphIcon
-                {...omit(this.props, ['direction', 'opened'])}
-                onMouseEnter={this.handleMouseEnter}
-                onMouseLeave={this.handleMouseLeave}
-                type={type}
-            />
-        )
-    }
+		return (
+			<MorphIcon
+				{...omit(this.props, ["direction", "opened"])}
+				onMouseEnter={this.handleMouseEnter}
+				onMouseLeave={this.handleMouseLeave}
+				type={type}
+			/>
+		);
+	}
 }
 
 NavButton.propTypes = {
-    direction: PropTypes.oneOf(['up', 'right', 'down', 'left']),
-    opened: PropTypes.bool.isRequired,
-}
+	direction: PropTypes.oneOf(["up", "right", "down", "left"]),
+	opened: PropTypes.bool.isRequired
+};
 
-NavButton.defaultProps = {
-    direction: 'down',
-    opened: false,
-}
-
-export default NavButton
+export default NavButton;
--- src/components/buttons/PlayButton.js
+++ src/components/buttons/PlayButton.js
@@ -6,62 +6,57 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import MorphIcon from '../MorphIcon'
-import omit from '../../lib/omit'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import MorphIcon from "../MorphIcon";
+import omit from "../../lib/omit";
 
 class PlayButton extends Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props);
 
-        this.handleMouseEnter = this.handleMouseEnter.bind(this)
-        this.handleMouseLeave = this.handleMouseLeave.bind(this)
+		this.handleMouseEnter = this.handleMouseEnter.bind(this);
+		this.handleMouseLeave = this.handleMouseLeave.bind(this);
 
-        this.state = { hover: false }
-    }
+		this.state = { hover: false };
+	}
 
-    handleMouseEnter() {
-        this.setState({ hover: true })
-    }
+	handleMouseEnter() {
+		this.setState({ hover: true });
+	}
 
-    handleMouseLeave() {
-        this.setState({ hover: false })
-    }
+	handleMouseLeave() {
+		this.setState({ hover: false });
+	}
 
-    render() {
-        const { isPlaying, circle } = this.props
-        const { hover } = this.state
+	render() {
+		const { isPlaying = false, circle = false } = this.props;
+		const { hover } = this.state;
 
-        let type
-        if (hover) {
-            type = isPlaying ? 'pauseSparks' : 'playSparks'
-        } else {
-            type = isPlaying ? 'pause' : 'play'
-            if (circle) {
-                type = `${type}Circle`
-            }
-        }
+		let type;
+		if (hover) {
+			type = isPlaying ? "pauseSparks" : "playSparks";
+		} else {
+			type = isPlaying ? "pause" : "play";
+			if (circle) {
+				type = `${type}Circle`;
+			}
+		}
 
-        return (
-            <MorphIcon
-                {...omit(this.props, ['isPlaying', 'circle'])}
-                onMouseEnter={this.handleMouseEnter}
-                onMouseLeave={this.handleMouseLeave}
-                type={type}
-            />
-        )
-    }
+		return (
+			<MorphIcon
+				{...omit(this.props, ["isPlaying", "circle"])}
+				onMouseEnter={this.handleMouseEnter}
+				onMouseLeave={this.handleMouseLeave}
+				type={type}
+			/>
+		);
+	}
 }
 
 PlayButton.propTypes = {
-    isPlaying: PropTypes.bool.isRequired,
-    circle: PropTypes.bool.isRequired,
-}
+	isPlaying: PropTypes.bool.isRequired,
+	circle: PropTypes.bool.isRequired
+};
 
-PlayButton.defaultProps = {
-    isPlaying: false,
-    circle: false,
-}
-
-export default PlayButton
+export default PlayButton;
